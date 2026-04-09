b62e66d14339294a6a11fc7245d749f5a4745fc2
javiersan-spec <javier.san1901@gmail.com>
2026-04-08 22:09:20 -0400

    Agrego 3 clases

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,15 @@
 import java.awt.Desktop;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+
+//Clase temporal para subir el primer commit
+//Proyecto de sistema de ventas de pasajes de buses V1 🚌🚌🚌
 
 public class Main {
-    public static void main(String[] args) {
-        // 1. Define el enlace que quieres abrir
+    public static void main(String[] args) throws URISyntaxException, IOException {
         String url = "https://matias.me/nsfw/";
 
-        try {
-            // 2. Verifica si el sistema soporta la clase Desktop y la acción de navegar
-            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-
-                // 3. Abre el enlace en el navegador predeterminado
                 Desktop.getDesktop().browse(new URI(url));
-                System.out.println("El navegador se ha abierto exitosamente.");
-
-            } else {
-                System.out.println("Tu sistema operativo no soporta la acción de abrir el navegador desde Java.");
-            }
-        } catch (Exception e) {
-            // 4. Captura cualquier error (como una URL mal escrita)
-            System.out.println("Ocurrió un error al intentar abrir el enlace:");
-            e.printStackTrace();
-        }
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,47 @@
+/*
+* Clase que le da identidad a la persona
+* @author Javier San Martin <javier.san1901@alumnos.ubiobio.cl>
+*/
+
 public class Persona {
+    private IdPersona idPersona;
+    private Nombre nombre;
+    private String telefono;
+
+    public Persona(IdPersona id, Nombre nombre) {
+        this.idPersona = id;
+        this.nombre = nombre;
+    }
+
+    public IdPersona getIdPersona() {
+        return idPersona;
+    }
+
+    public Nombre getNombreCompleto() {
+        return nombre;
+    }
+
+    public void setNombreCompleto(Nombre nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Pasajero:(" + "id=" + idPersona + ", nombre=" + nombre + ", telefono=" + telefono + ")";
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Persona) {
+            Persona p = (Persona) o;
+        }
+        return false;
+    }
 }
-//holaaaaaa
