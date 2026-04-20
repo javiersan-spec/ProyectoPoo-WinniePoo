d4b78bb91bf29aea22cb555d3dca04c15dd06415
genesis.castro-spec <genesis.castro2401@alumnos.ubiobio.cl>
2026-04-20 16:03:39 -0400

    termine clase viaje bus y pasajero

--- src/Viaje.java
+++ src/Viaje.java
@@ -1,2 +1,20 @@
-public class Viaje {
+public class Pasajero extends Persona {
+
+    private Nombre nomContacto;
+    private String fonoContacto;
+
+    public Pasajero(String id, Nombre nombre, String telefono,
+                    Nombre nomContacto, String fonoContacto) {
+        super(id, nombre, telefono);
+        this.nomContacto = nomContacto;
+        this.fonoContacto = fonoContacto;
+    }
+
+    public Nombre getNomContacto() {
+        return nomContacto;
+    }
+
+    public String getFonoContacto() {
+        return fonoContacto;
+    }
 }
