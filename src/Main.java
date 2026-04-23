674976cb9d2616e61fce8b9c8fc5ba7fe1b5aaa2
javiersan-spec <javier.san1901@gmail.com>
2026-04-22 21:13:07 -0400

    avanzo con la clase main

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,34 @@ import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
-    private static SistemaVentaPasajes sistema =  new SistemaVentaPasajes();
-    private static Scanner  leer = new Scanner(System.in);
-    private static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    private static DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-    public static void main(String[] args) {
+    private static SistemaVentaPasajes sistema;
+    private static Scanner leerTeclado;
+    private static DateTimeFormatter dateFormatter;
+    private static DateTimeFormatter timeFormatter;
 
+    public Main () {
+        sistema = new SistemaVentaPasajes();
+        leerTeclado = new Scanner(System.in);
+        dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+    }
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.menu();
+    }
+    private void menu() {
+        int opcion = -1;
+        while (opcion != 0) {
+            System.out.println("\n...::: Menú principal :::...");
+            System.out.println("1. Crear cliente");
+            System.out.println("2. Crear Bus");
+            System.out.println("3. Crear Viaje");
+            System.out.println("4. Lista Venta de Pasajes");
+            System.out.println("5. Lista de pasajeros");
+            System.out.println("6. Lista de ventas");
+            System.out.println("0. Salir");
+            System.out.print("Ingrese opcion: ");
+        }
     }
 }
+
