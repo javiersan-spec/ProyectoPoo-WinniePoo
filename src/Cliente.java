a49ec85d0020a6290cd48834f4480405db6206f9
benja <benjamin.jara2301@alumnos.ubiobio.cl>
2026-04-25 20:28:51 -0400

    arreglando la clase cliente (faltaba la lista de ventas y el getter)

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Clase encargada de generar una clase hijo de persona
  * @author Beatriz Aguilera
  */
-
 public class Cliente extends Persona {
     private String email;
+    private List<Venta> ventas;
 
     public Cliente(IdPersona id, Nombre nom, String email) {
         super(id, nom);
         this.email = email;
+        this.ventas = new ArrayList<>();
     }
 
     public String getEmail() {
@@ -18,7 +21,12 @@ public class Cliente extends Persona {
     public void setEmail(String email) {
         this.email = email;
     }
-
     public void addVenta(Venta venta) {
+        if (venta != null) {
+            this.ventas.add(venta);
+        }
+    }
+    public Venta[] getVentas() {
+        return ventas.toArray(new Venta[0]);
     }
 }
