8ca18f9458343400443d1374bc9af68b5bc99262
benja <benjamin.jara2301@alumnos.ubiobio.cl>
2026-05-10 22:51:08 -0400

    Clase auxiliar completa

--- src/modelo/Auxiliar.java
+++ src/modelo/Auxiliar.java
@@ -3,5 +3,24 @@ package modelo;
  * @author Benjamin Jara
  */
 
-public class Auxiliar {
+public class Auxiliar extends Tripulante {
+
+    private int nroViajes;
+
+    public Auxiliar(IdPersona id, Nombre nom, Direccion dir) {
+        super(id, nom, dir);
+        this.nroViajes = 0;
+    }
+
+    @Override
+    public void addViaje(Viaje viaje) {
+        if (viaje != null) {
+            this.nroViajes++;
+        }
+    }
+
+    @Override
+    public int getNroViajes() {
+        return this.nroViajes;
+    }
 }
