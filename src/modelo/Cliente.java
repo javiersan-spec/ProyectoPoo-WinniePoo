60375e963194ead5042754ccfaf87ea0e9a32800
genesis.castro-spec <genesis.castro2401@alumnos.ubiobio.cl>
2026-05-13 00:41:16 -0400

    corregida y terminada
    Signed-off-by: genesis.castro-spec <genesis.castro2401@alumnos.ubiobio.cl>

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -31,6 +31,6 @@ public class Cliente extends Persona {
             nuevasVentas[i] = ventas[i];
         }
         nuevasVentas[ventas.length] = venta;
-        ventas = nuevas
+        ventas = nuevasVentas;
     }
 }
