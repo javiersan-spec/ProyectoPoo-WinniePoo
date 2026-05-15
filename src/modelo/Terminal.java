33f567fcc533dfff325257babaf398124f2378e6
benja <benjamin.jara2301@alumnos.ubiobio.cl>
2026-05-14 23:37:42 -0400

    implemente la clase terminal con gestion independiente de viajes de llegada y salida siguiendo el uml

--- src/modelo/Terminal.java
+++ src/modelo/Terminal.java
@@ -3,6 +3,51 @@ package modelo;
  * @author Benjamin Jara
  */
 
-public class Terminal {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Terminal {
+    private String nombre;
+    private Direccion direccion;
+    private List<Viaje> llegadas;
+    private List<Viaje> salidas;
+
+    public Terminal(String nombre, Direccion direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.llegadas = new ArrayList<>();
+        this.salidas = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public void addLlegada(Viaje viaje) {
+        if (viaje != null) {
+            this.llegadas.add(viaje);
+        }
+    }
+
+    public void addSalida(Viaje viaje) {
+        if (viaje != null) {
+            this.salidas.add(viaje);
+        }
+    }
+
+    public Viaje[] getLlegadas() {
+        return llegadas.toArray(new Viaje[0]);
+    }
+
+    public Viaje[] getSalidas() {
+        return salidas.toArray(new Viaje[0]);
+    }
 }
